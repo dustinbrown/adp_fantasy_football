5548cd5aa78a563945d9aaeb27b6ac0b3a7025a7
deeje <dustinjamesbrown@gmail.com>
2014-08-20 18:05:10 -0400

    removed debug statement and fixed whitespace

--- bin/app.rb
+++ bin/app.rb
@@ -23,11 +23,11 @@ def populate_arrays
 	#Create my_players array with adp plus player details
 	# I'm sure there are 15 better ways to do this..
 	players['players']['player'].each do |player|
-	  adp['adp']['player'].each do |adp|
-	    if adp['id'] == player['id']
-	      @my_players << { :name => "#{player['name']}", :adp => "#{adp['averagePick']}".to_f, :position => "#{player['position']}", :team => "#{player['team']}", :id => "#{player['id']}" }
-	    end
-	  end
+		adp['adp']['player'].each do |adp|
+			if adp['id'] == player['id']
+				@my_players << { :name => "#{player['name']}", :adp => "#{adp['averagePick']}".to_f, :position => "#{player['position']}", :team => "#{player['team']}", :id => "#{player['id']}" }
+			end
+		end
 	end
 
 end
@@ -44,64 +44,61 @@ get '/populate_array/' do
 	populate_arrays()
 	my_players = @my_players
 	drafted_players = []
-  redirect "/"
+	redirect "/"
 end
 
 #listens on port 8080 -- splash page
 get '/' do
-  #Show adp webpage
-  erb :adp, :locals => {'players' => my_players}
+	#Show adp webpage
+	erb :adp, :locals => {'players' => my_players}
 end
 
 #Remove player from available list
 post '/draft/' do
 
-	puts "DEBUG: #{params[:nameid]}"
 	#Add player to drafted_players array
 	drafted_players << my_players.select { |player| player[:id] ==  params[:nameid] }.first if ! my_players.select { |player| player[:id] ==  params[:nameid] }.empty?
 	
   #Remove player from array -- uses id from button clicked
-  my_players.delete_if {|player| player[:id] == params[:nameid] }
+	my_players.delete_if {|player| player[:id] == params[:nameid] }
 
-  #Show adp webpage
-  erb :adp, :locals => {'players' => my_players}
+	#Show adp webpage
+	erb :adp, :locals => {'players' => my_players}
 end
 
 get '/drafted/' do
-  
 	erb :drafted, :locals => {'players' => drafted_players}
-
 end
 
 post '/undraft/' do
 	#Add player to drafted_players array
 	if my_players.select { |player| player[:id] ==  params[:nameid] }.empty?
 		my_players << drafted_players.select { |player| player[:id] ==  params[:nameid] }.first
-  	
+
 		#Remove player from array -- uses id from button clicked
-  	drafted_players.delete_if {|player| player[:id] == params[:nameid] }
+		drafted_players.delete_if {|player| player[:id] == params[:nameid] }
 	end
 
-  #Show adp webpage
-  erb :adp, :locals => {'players' => my_players}
-  
+	#Show adp webpage
+	erb :adp, :locals => {'players' => my_players}
+
 end
 
 #Show players by position
 post '/position/' do
-  #Uses post parameter via button clicked
-  position = params[:position].upcase
+	#Uses post parameter via button clicked
+	position = params[:position].upcase
 
-  #Set up position only array
-  position_array = []
+	#Set up position only array
+	position_array = []
 
-  #Insert players into array if they match position passed
-  my_players.each do |player|
-    if player[:position].upcase =~ /#{position}/
-      position_array << player
-    end
-  end
+	#Insert players into array if they match position passed
+	my_players.each do |player|
+		if player[:position].upcase =~ /#{position}/
+			position_array << player
+		end
+	end
 
-  #Show adp webpage
-  erb :adp, :locals => {'players' => position_array}
+	#Show adp webpage
+	erb :adp, :locals => {'players' => position_array}
 end
