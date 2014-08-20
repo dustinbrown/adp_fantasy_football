4de1e1eebfff0810986356f7b7529dd1281c6301
deeje <dustinjamesbrown@gmail.com>
2014-08-20 17:58:42 -0400

    added app reload logic with hidden endpoint

--- bin/app.rb
+++ bin/app.rb
@@ -8,23 +8,43 @@ set :static, true
 set :public_folder, "static"
 set :views, "views"
 
-#get list of players in json
-players = JSON.parse(open("http://football.myfantasyleague.com/2014/export?TYPE=players&L=&W=&JSON=1").read)
-
-#get list of adp
-adp = JSON.parse(open("http://football.myfantasyleague.com/2014/export?TYPE=adp&L=&W=&JSON=1").read)
-
-my_players = []
 drafted_players = []
 
-#Create my_players array with adp plus player details
-# I'm sure there are 15 better ways to do this..
-players['players']['player'].each do |player|
-  adp['adp']['player'].each do |adp|
-    if adp['id'] == player['id']
-      my_players << { :name => "#{player['name']}", :adp => "#{adp['averagePick']}".to_f, :position => "#{player['position']}", :team => "#{player['team']}", :id => "#{player['id']}" }
-    end
-  end
+
+def populate_arrays
+	@my_players = Array.new
+
+	#get list of players in json
+	players = JSON.parse(open("http://football.myfantasyleague.com/2014/export?TYPE=players&L=&W=&JSON=1").read)
+	
+	#get list of adp
+	adp = JSON.parse(open("http://football.myfantasyleague.com/2014/export?TYPE=adp&L=&W=&JSON=1").read)
+
+	#Create my_players array with adp plus player details
+	# I'm sure there are 15 better ways to do this..
+	players['players']['player'].each do |player|
+	  adp['adp']['player'].each do |adp|
+	    if adp['id'] == player['id']
+	      @my_players << { :name => "#{player['name']}", :adp => "#{adp['averagePick']}".to_f, :position => "#{player['position']}", :team => "#{player['team']}", :id => "#{player['id']}" }
+	    end
+	  end
+	end
+
+end
+
+#Set up web app
+populate_arrays()
+
+my_players = @my_players
+
+#Endpoints#
+
+#Essentially a reset without restarting application -- no ui button for this
+get '/populate_array/' do
+	populate_arrays()
+	my_players = @my_players
+	drafted_players = []
+  redirect "/"
 end
 
 #listens on port 8080 -- splash page
